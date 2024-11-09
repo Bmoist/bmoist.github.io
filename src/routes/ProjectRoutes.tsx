0759fc6be0f627e13bdcf3f328a70891f8f2805d
Linzan <lye11@u.rochester.edu>
2024-11-09 17:21:22 +0800

    developed About page and ChordSense page

--- src/routes/ProjectRoutes.tsx
+++ src/routes/ProjectRoutes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Maskexp from "../projects/Maskexp";
 import Notfound from "../pages/Notfount";
+import ChordSense from "../projects/ChordSense";
 
 const ProjectRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Notfound />} />
       <Route path="maskexp" element={<Maskexp />} />
+      <Route path="chordsense" element={<ChordSense />} />
     </Routes>
   );
 };
